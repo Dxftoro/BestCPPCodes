c5cb657a0c20fd1e3c68bb3b686dc51ab37f6906
Dxftoro <96373847+Dxftoro@users.noreply.github.com>
2024-09-14 10:46:30 +0300

    Update Transliterator.cpp

--- Transliterator.cpp
+++ Transliterator.cpp
@@ -121,31 +121,3 @@ int main() { //мейн... -_-
 	system("pause");
 	return 1;
 }
-
-#include "stdio.h"
-
-int tobin(int x) {
-    char str[5];
-    int i = 5;
-    
-    while (x / 2 != 0) {
-        i--;
-        x = x / 2;
-        str[i] = (x % 2 + '0');
-    }
-    
-    int cnt = 0;
-    for (int i =0; i < 5; i++) {
-        if (str[i] == '1') cnt++;
-    }
-    
-    return cnt;
-}
-
-int main() {
-    int n;
-    scanf("%d", &n);
-    printf("%d", tobin(n));
-
-    return 0;
-}
